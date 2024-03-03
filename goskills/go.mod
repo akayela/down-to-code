c2a05b343d53e385d298d1a989d652c2449f7080
akayela <akayela22@gmail.com>
2024-03-03 08:56:11 +0200

    Down to code in go

---
+++ goskills/go.mod
@@ -0,0 +1,3 @@
+module github.com/akayela/goskills
+
+go 1.21.5
---
+++ goskills/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "log"
+  "os"
+  "strconv"
+) 
+
+func getFloats(fileName string) ( []float64, error) {
+  var numbers []float64
+  file, err := os.Open(fileName)
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    line := scanner.Text()
+    number, err := strconv.ParseFloat(line, 64)
+    if err != nil {
+      log.Fatal(err)
+    }
+    numbers = append(numbers, number)
+  }
+  err = file.Close()
+  if scanner.Err() != nil {
+    log.Fatal(err)
+  }
+  return numbers, nil
+}
+
+func main() {
+  numbers, err := getFloats("./floats.txt")
+  if err != nil {
+    log.Fatal(err)
+  }
+  var sum float64 = 0
+  for _, number := range numbers {
+    sum += number
+  }
+  fmt.Printf("Average is: %0.2f\n", sum/float64(len(numbers)))
+}
+  
